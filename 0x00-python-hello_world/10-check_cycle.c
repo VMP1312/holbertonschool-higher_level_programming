4780db2ed078efa93c268c8a6aa0316c7ec2dd95
VMP1312 <victor.paz1312@hotmail.com>
2020-04-27 07:43:37 +0000

    Create

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * check_cycle - Checks if a singly linked list has a cycle in it.
+ * @list: list.
+ * Return: 0 if there is not a cycle, otherwise 1.
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *tortuga = list;
+	listint_t *liebre = list;
+
+	while (tortuga != NULL && liebre != NULL)
+	{
+		tortuga = tortuga->next;
+		liebre = liebre->next->next;
+		if (tortuga == liebre)
+		{
+			return (1);
+		}
+	}
+	return (0);
+
+}
