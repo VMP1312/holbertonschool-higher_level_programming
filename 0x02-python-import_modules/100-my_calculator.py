40b3b6cca481059c56ebdd3ed4cae3188c0d6a79
VMP1312 <victor.paz1312@hotmail.com>
2020-04-30 19:57:47 +0000

    Fix

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -10,13 +10,13 @@ if __name__ == "__main__":
     num2 = int(argv[3])
     operator = argv[2]
 
-    if(operator == "+"):
+    if(operator == '+'):
         print("{} + {} = {}".format(num1, num2, add(num1, num2)))
-    elif(operator == "-"):
+    elif(operator == '-'):
         print("{} - {} = {}".format(num1, num2, sub(num1, num2)))
-    elif(operator == "*"):
+    elif(operator == '*'):
         print("{} * {} = {}".format(num1, num2, mul(num1, num2)))
-    elif(operator == "/"):
+    elif(operator == '/'):
         print("{} / {} = {}".format(num1, num2, div(num1, num2)))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
