4b8673540ba1f8b9e7f2c540aaa7f7983e063b98
VMP1312 <victor.paz1312@hotmail.com>
2020-04-30 07:42:36 +0000

    Fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-import sys
-if len(sys.argv) == 1:
-    print("0 arguments.")
-else:
-    if len(sys.argv) == 2:
-        print("1 argument:")
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) == 1:
+        print("0 arguments.")
     else:
-        print("{:d} arguments:".format(len(sys.argv) - 1))
-    for argv in range(1, len(sys.argv)):
-        print("{:d}: {}".format(argv, (sys.argv[argv])))
+        if len(sys.argv) == 2:
+            print("1 argument:")
+        else:
+            print("{:d} arguments:".format(len(sys.argv) - 1))
+        for argv in range(1, len(sys.argv)):
+            print("{:d}: {}".format(argv, (sys.argv[argv])))
