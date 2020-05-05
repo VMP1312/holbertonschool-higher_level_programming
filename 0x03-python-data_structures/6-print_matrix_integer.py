87c7df5f76266ab3a1209980b0775fcddd016fc2
VMP1312 <victor.paz1312@hotmail.com>
2020-05-05 22:16:31 +0000

    fix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,8 +3,8 @@ def print_matrix_integer(matrix=[[]]):
     for x in matrix:
         y = 1
         for cnt in x:
-            print("{:d}".format(cnt), end=" ")
+            print("{:d}".format(cnt), end="")
             if len(x) > y:
-                print(end="")
+                print(end=" ")
             y += 1
         print(end="\n")
