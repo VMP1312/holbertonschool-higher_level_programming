61735d008a0b069e2ccae21cde3a0f7bde9fc2ea
VMP1312 <victor.paz1312@hotmail.com>
2020-05-25 23:26:56 +0000

    Fix

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -1,49 +1,58 @@
 #!/usr/bin/python3
 """
 Prints a rectangle
-Use '#' to draw.
-Return: None
 """
 
 
-class Rectangle():
+class Rectangle:
+    """Class Rectangle"""
+
     def __init__(self, width=0, height=0):
-        self.__height = height
-        self.__width = width
+        """Initialize"""
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
+        """Width"""
         return self.__width
 
     @property
     def height(self):
+        """Height"""
         return self.__height
 
     @width.setter
     def width(self, value):
-        if type(value) != int:
+        """Width setter"""
+        if not(isinstance(value, int)):
             raise TypeError("width must be an integer")
-        if value < 0:
+        if(value < 0):
             raise ValueError("width must be >= 0")
         self.__width = value
 
     @height.setter
     def height(self, value):
-        if type(value) != int:
+        """Height setter"""
+        if not(isinstance(value, int)):
             raise TypeError("height must be an integer")
-        if value < 0:
+        if(value < 0):
             raise ValueError("height must be >= 0")
         self.__height = value
 
     def area(self):
+        """Area"""
         return self.height * self.width
 
     def perimeter(self):
-        if self.width is not 0 or self.height is not 0:
+        """Perimeter"""
+        if self.width is 0 or self.height is 0:
+            return 0
+        else:
             return (self.width + self.height) * 2
-        return 0
 
     def __str__(self):
+        """String"""
         string = ""
         if self.height is 0 or self.width is 0:
             return ""
