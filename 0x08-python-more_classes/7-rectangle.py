19fbb729c1bf2647c9271ee5de0d95f92d00db86
VMP1312 <victor.paz1312@hotmail.com>
2020-05-25 23:48:15 +0000

    Fix

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -18,49 +18,59 @@ class Rectangle:
 
     @property
     def width(self):
+        """Width"""
         return self.__width
 
     @property
     def height(self):
+        """Height"""
         return self.__height
 
     @width.setter
     def width(self, value):
-        if type(value) != int:
+        """Width setter"""
+        if not(isinstance(value, int)):
             raise TypeError("width must be an integer")
-        if value < 0:
+        if(value < 0):
             raise ValueError("width must be >= 0")
         self.__width = value
 
     @height.setter
     def height(self, value):
-        if type(value) != int:
+        """Height setter"""
+        if not(isinstance(value, int)):
             raise TypeError("height must be an integer")
-        if value < 0:
+        if(value < 0):
             raise ValueError("height must be >= 0")
         self.__height = value
 
     def area(self):
+        """Area"""
         return self.height * self.width
 
     def perimeter(self):
-        if self.width is not 0 or self.height is not 0:
+        """Perimeter"""
+        if self.width is 0 or self.height is 0:
+            return 0
+        else:
             return (self.width + self.height) * 2
-        return 0
 
     def __str__(self):
+        """String"""
         string = ""
         if self.height is 0 or self.width is 0:
-            return string
+            return ""
         for row in range(self.height):
             for clm in range(self.width):
-                string += str(self.prints_ymbol)
+                string += str(self.print_symbol)
             string += '\n'
-        return string[:-1]
+        return string[0:-1]
 
     def __repr__(self):
+        """Representation"""
         return "Rectangle({}, {})".format(self.width, self.height)
 
     def __del__(self):
+        """Check delete"""
         Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
