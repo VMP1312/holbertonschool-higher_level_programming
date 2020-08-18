441f04ce046306fd278c33c0ee58d7017a91ad33
VMP1312 <victor.paz1312@hotmail.com>
2020-08-18 15:53:05 -0500

    Fix

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Displays all HTTP methods the server will accept.
-curl -sI -X OPTIONS "$1" | grep Allow | cut -d' ' -f1-
+curl -sIX OPTIONS "$1" | grep -e Allow | cut -d":" -f2 | sed -e 's/^ //'
