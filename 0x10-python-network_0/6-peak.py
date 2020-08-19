8d2d6ae57bbe7e6c933c180bd1e7af3d82572b53
VMP1312 <victor.paz1312@hotmail.com>
2020-08-18 21:12:02 -0500

    Fix

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+finds a peak in a list of unsorted integers.
+"""
+
+
+def find_peak(list_of_integers):
+    """Find Peak"""
+    if not list_of_integers:
+        return None
+    peak = find(list_of_integers, 0, len(list_of_integers) - 1,
+                len(list_of_integers))
+    return peak
+
+
+def find(lst, low, high, maxi):
+    """Find Peak"""
+    check = low + (high - low)/2
+    check = int(check)
+
+    if (check == 0 or lst[check - 1] <= lst[check]) and\
+       (check == maxi - 1 or lst[check + 1] <= lst[check]):
+        return lst[check]
+
+    elif (check > 0 and lst[check + 1] > lst[check]):
+        return find(lst, check + 1, high, maxi)
+    else:
+        return find(lst, low, check - 1, maxi)
