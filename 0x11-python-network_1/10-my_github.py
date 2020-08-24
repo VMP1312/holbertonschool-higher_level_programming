730c8b4367aa1f91b09e3a0eba5258f6311a0a91
VMP1312 <victor.paz1312@hotmail.com>
2020-08-24 18:35:14 -0500

    Fix

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -5,13 +5,12 @@ Takes your Github credentials and uses the Github API to display your id.
 
 import requests
 import sys
-from requests.auth import HTTPBasicAuth
+
 
 if __name__ == "__main__":
 
-    response = requests.get('https://api.github.com/user',
-                            auth=(HTTPBasicAuth(argv[1], argv[2])))
-
+    cred = (sys.argv[1], sys.argv[2])
+    response = requests.get('https://api.github.com/user', auth=cred)
     if "json" not in response.headers.get('content-type'):
         print("Not a valid JSON")
     else:
